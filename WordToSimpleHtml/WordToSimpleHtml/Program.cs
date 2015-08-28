256144dee6fb23a467f34ea17e45a6c00d452839
Ted Johnson <johnson520@gmail.com>
2015-08-28 12:09:02 -0700

    Don't target blank relative URLs

--- WordToSimpleHtml/WordToSimpleHtml/Program.cs
+++ WordToSimpleHtml/WordToSimpleHtml/Program.cs
@@ -8,31 +8,29 @@ namespace WordToSimpleHtml
 	{
 		static void Main(string[] args)
 		{
-			string title;
-
-			if (args.Length != 2)
+		    if (args.Length != 2)
 			{
 				Console.WriteLine("Usage: word-file output-file");
 				return;
 			}
 
-			var docxFile = System.IO.Path.GetFullPath(args[0]);
-			var htmlFile = System.IO.Path.GetFullPath(args[1]);
-// ReSharper disable AssignNullToNotNullAttribute
+			var docxFile = Path.GetFullPath(args[0]);
+			var htmlFile = Path.GetFullPath(args[1]);
+
+            htmlFile = Path.GetDirectoryName(htmlFile) + @"\" + Regex.Replace(Path.GetFileName(htmlFile), @"\s+", "-").ToLowerInvariant();
 			var imageFilePrefix = Regex.Replace(Path.GetFileNameWithoutExtension(htmlFile), @"\W", string.Empty) + "-";
-// ReSharper restore AssignNullToNotNullAttribute
 
 			Console.WriteLine("Converting {0} to {1} with images to {2}", docxFile, htmlFile, imageFilePrefix);
 
 			try
 			{
-				new DocxToHtml(Console.WriteLine).Convert(docxFile, htmlFile, imageFilePrefix, out title);
+			    string title;
+			    new DocxToHtml(Console.WriteLine).Convert(docxFile, htmlFile, imageFilePrefix, out title);
 			}
-			catch (System.IO.IOException ioe)
+			catch (IOException ioe)
 			{
 				Console.WriteLine(Environment.NewLine + "!!!" + ioe.Message);
 			}
-
 
 		}
 	}
